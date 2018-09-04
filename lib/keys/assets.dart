b150e4d6be4cd746becf2c781438b8ff9c9bc49f
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-09-04 16:06:04 +0200

    Add icons (Android (normal/adaptive icon and for iOS) Update gradle Add real splashscreen (flutter splashscreen only to load preferences)

--- lib/keys/assets.dart
+++ lib/keys/assets.dart
@@ -2,7 +2,7 @@ class Asset {
   static const BASE_PATH = 'res/';
   static const IMG_PATH = '${BASE_PATH}images/';
 
-  static const LOGO = '${IMG_PATH}logo.png';
+  static const LOGO = '${IMG_PATH}icon.png';
   static const PICTURE_JC = '${IMG_PATH}jeancharles-mousse.png';
   static const GITHUB_DARK = '${IMG_PATH}GitHub-Mark.png';
   static const GITHUB_WHITE = '${IMG_PATH}GitHub-Mark-Light.png';
--- lib/screens/splashscreen/splashscreen.dart
+++ lib/screens/splashscreen/splashscreen.dart
@@ -1,18 +1,13 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:myagenda/keys/assets.dart';
 import 'package:myagenda/keys/route_key.dart';
 import 'package:myagenda/utils/dynamic_theme.dart';
 import 'package:myagenda/utils/functions.dart';
 import 'package:myagenda/utils/preferences.dart';
-import 'package:myagenda/widgets/ui/circular_loader.dart';
 
 class SplashScreen extends StatelessWidget {
   Future<bool> _initPreferences(BuildContext context) async {
-    // Store datetime start
-    final DateTime startTime = DateTime.now();
-
     // Get all data to setup theme
     final bool isDark = await Preferences.getDarkTheme();
     final int appbarColor = await Preferences.getPrimaryColor();
@@ -36,13 +31,6 @@ class SplashScreen extends StatelessWidget {
 
     final bool isFirstBoot = await Preferences.isFirstBoot();
 
-    // Get date when all prefs setup
-    final int diffInMs = DateTime.now().difference(startTime).inMilliseconds;
-    // Get wait time (1 sec is max)
-    final int waitTime = 1000 - diffInMs;
-    await Future.delayed(
-        Duration(milliseconds: waitTime > 0 ? waitTime : 0), null);
-
     return isFirstBoot;
   }
 
@@ -53,19 +41,6 @@ class SplashScreen extends StatelessWidget {
       Navigator.of(context).pushReplacementNamed(routeDest);
     });
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(Asset.LOGO, width: 150.0),
-            const Padding(
-              padding: const EdgeInsets.only(top: 90.0),
-              child: const CircularLoader(),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }
