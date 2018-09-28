c4f34b36a6ac700f4e565e044bb02afc296776eb
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-09-28 02:08:21 +0200

    Fix warning convention

--- lib/models/courses/weekday.dart
+++ lib/models/courses/weekday.dart
@@ -3,20 +3,20 @@ class WeekDay {
 
   const WeekDay._(this.weekDay);
 
-  static final MONDAY = WeekDay._(DateTime.monday);
-  static final TUESDAY = WeekDay._(DateTime.tuesday);
-  static final WEDNESDAY = WeekDay._(DateTime.wednesday);
-  static final THURSDAY = WeekDay._(DateTime.thursday);
-  static final FRIDAY = WeekDay._(DateTime.friday);
-  static final SATURDAY = WeekDay._(DateTime.saturday);
-  static final SUNDAY = WeekDay._(DateTime.sunday);
+  static final monday = WeekDay._(DateTime.monday);
+  static final tuesday = WeekDay._(DateTime.tuesday);
+  static final wednesday = WeekDay._(DateTime.wednesday);
+  static final thursday = WeekDay._(DateTime.thursday);
+  static final friday = WeekDay._(DateTime.friday);
+  static final saturday = WeekDay._(DateTime.saturday);
+  static final sunday = WeekDay._(DateTime.sunday);
 
   get value => weekDay;
 
   static get count => values.length;
 
   static List<WeekDay> get values =>
-      [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY];
+      [monday, tuesday, wednesday, thursday, friday, saturday, sunday];
 
   factory WeekDay.fromIndex(int value) => values[value - 1];
 }
