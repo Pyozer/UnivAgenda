5004529d04c6dbc5c562ad5957d2ba58c99f7da1
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-10-03 00:15:33 +0200

    Improve dialog design

--- lib/widgets/ui/dialog/simple_alert_dialog.dart
+++ lib/widgets/ui/dialog/simple_alert_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myagenda/widgets/ui/raised_button_colored.dart';
 
 class SimpleAlertDialog extends StatelessWidget {
   static final boldText = const TextStyle(fontWeight: FontWeight.w600);
@@ -15,22 +16,27 @@ class SimpleAlertDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      titlePadding: const EdgeInsets.all(20.0),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
       title: Text(title, style: boldText),
-      content: Text(text),
+      content: Text(text, textAlign: TextAlign.justify),
       actions: <Widget>[
         (btnNegative != null)
             ? FlatButton(
-                child: Text(btnNegative, style: boldText),
+                child: Text(btnNegative.toUpperCase(), style: boldText),
                 onPressed: () {
                   Navigator.of(context).pop(false);
                 },
               )
             : const SizedBox.shrink(),
-        FlatButton(
-          child: Text(btnPositive, style: boldText),
-          onPressed: () {
-            Navigator.of(context).pop(true);
-          },
+        Padding(
+          padding: const EdgeInsets.only(right: 6.0),
+          child: RaisedButtonColored(
+            text: btnPositive,
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+          ),
         ),
       ],
     );
