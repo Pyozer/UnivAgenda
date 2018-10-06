5d440f8e1a2d7997ff73939540400adb06de4674
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-10-06 18:53:44 +0200

    Improve add custom event layout (design and UX)

--- lib/screens/custom_event/custom_event.dart
+++ lib/screens/custom_event/custom_event.dart
@@ -9,6 +9,7 @@ import 'package:myagenda/utils/translations.dart';
 import 'package:myagenda/widgets/settings/list_tile_color.dart';
 import 'package:myagenda/widgets/ui/circle_text.dart';
 import 'package:myagenda/widgets/ui/dialog/dialog_predefined.dart';
+import 'package:myagenda/widgets/ui/list_divider.dart';
 import 'package:outline_material_icons/outline_material_icons.dart';
 import 'package:uuid/uuid.dart';
 
@@ -22,8 +23,6 @@ class CustomEventScreen extends StatefulWidget {
 }
 
 class _CustomEventScreenState extends State<CustomEventScreen> {
-  final _formKey = GlobalKey<FormState>();
-
   final DateTime _firstDate = DateTime.now();
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descController = TextEditingController();
@@ -54,7 +53,7 @@ class _CustomEventScreenState extends State<CustomEventScreen> {
 
       _isEventRecurrent = widget.course.isRecurrentEvent();
       _selectedWeekdays = widget.course.weekdaysRepeat;
-      
+
       _isCustomColor = widget.course.color != null;
       _eventColor = widget.course.color ?? materialColors[0];
     } else {
@@ -72,6 +71,17 @@ class _CustomEventScreenState extends State<CustomEventScreen> {
     _descNode.dispose();
     _locationNode.dispose();
     super.dispose();
+  }
+
+  void _onRecurrentDate(bool value) {
+    setState(() {
+      _isEventRecurrent = value;
+    });
+  }
+  void _onColorCustom(bool value) {
+    setState(() {
+      _isCustomColor = value;
+    });
   }
 
   void _onDateTap() async {
@@ -126,54 +136,54 @@ class _CustomEventScreenState extends State<CustomEventScreen> {
     });
   }
 
-  Widget _buildDateTimeField(
-      String title, String value, GestureTapCallback onTap) {
-    return InkWell(
-      onTap: onTap,
-      child: TextField(
-        enabled: false,
-        autofocus: false,
-        controller: TextEditingController(text: value),
-        decoration: InputDecoration(labelText: title, border: InputBorder.none),
-      ),
+  Widget _buildDateTimeField(String title, String value) {
+    return TextField(
+      enabled: false,
+      autofocus: false,
+      controller: TextEditingController(text: value),
+      decoration: InputDecoration(labelText: title, border: InputBorder.none),
     );
   }
 
-  String _validateTextField(String value) {
-    if (value.isEmpty)
-      return Translations.of(context).get(StringKey.REQUIRE_FIELD);
-    return null;
+  void _onSubmit(BuildContext context) {
+    final translate = Translations.of(context);
+
+    if (_titleController.text.isEmpty ||
+        _descController.text.isEmpty ||
+        _locationController.text.isEmpty) {
+      _showError(translate.get(StringKey.REQUIRE_FIELD));
+      return;
+    }
+    if (_eventDateEnd.isBefore(_eventDateStart)) {
+      DialogPredefined.showEndTimeError(context);
+      return;
+    }
+    if (_isEventRecurrent && _selectedWeekdays.length == 0) {
+      _showError(translate.get(StringKey.ERROR_EVENT_RECURRENT_ZERO));
+      return;
+    }
+
+    final course = CustomCourse(
+      widget.course?.uid ?? Uuid().v1(),
+      _titleController.text,
+      _descController.text,
+      _locationController.text,
+      _eventDateStart,
+      _eventDateEnd,
+      notes: widget.course?.notes ?? [],
+      color: (_isCustomColor && _eventColor != null) ? _eventColor : null,
+      weekdaysRepeat: _isEventRecurrent ? _selectedWeekdays : [],
+    );
+
+    Navigator.of(context).pop(course);
   }
 
-  void _onSubmit(BuildContext context) {
-    if (_formKey.currentState.validate()) {
-      // Check data
-      if (_eventDateEnd.isBefore(_eventDateStart)) {
-        DialogPredefined.showEndTimeError(context);
-        return;
-      } else if (_isEventRecurrent && _selectedWeekdays.length == 0) {
-        final translate = Translations.of(context);
-        DialogPredefined.showSimpleMessage(
-          context,
-          translate.get(StringKey.ERROR),
-          translate.get(StringKey.ERROR_EVENT_RECURRENT_ZERO),
-        );
-        return;
-      }
-
-      final course = CustomCourse(
-          widget.course?.uid ?? Uuid().v1(),
-          _titleController.text,
-          _descController.text,
-          _locationController.text,
-          _eventDateStart,
-          _eventDateEnd,
-          notes: widget.course?.notes ?? [],
-          color: (_isCustomColor && _eventColor != null) ? _eventColor : null,
-          weekdaysRepeat: _isEventRecurrent ? _selectedWeekdays : []);
-
-      Navigator.of(context).pop(course);
-    }
+  void _showError(String msg) {
+    DialogPredefined.showSimpleMessage(
+      context,
+      Translations.of(context).get(StringKey.ERROR),
+      msg,
+    );
   }
 
   InputDecoration _buildInputDecoration(IconData icon, String hintText) {
@@ -239,133 +249,120 @@ class _CustomEventScreenState extends State<CustomEventScreen> {
         )
       ],
       body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                textInputAction: TextInputAction.next,
-                controller: _titleController,
-                decoration: _buildInputDecoration(
-                  OMIcons.title,
-                  translations.get(StringKey.TITLE_EVENT),
-                ),
-                validator: _validateTextField,
-                onEditingComplete: () =>
-                    FocusScope.of(context).requestFocus(_descNode),
+        child: Column(
+          children: [
+            TextFormField(
+              textInputAction: TextInputAction.next,
+              controller: _titleController,
+              decoration: _buildInputDecoration(
+                OMIcons.title,
+                translations.get(StringKey.TITLE_EVENT),
               ),
-              const Divider(height: 0.0),
-              TextFormField(
-                focusNode: _descNode,
-                textInputAction: TextInputAction.next,
-                controller: _descController,
-                decoration: _buildInputDecoration(
-                  OMIcons.description,
-                  translations.get(StringKey.DESC_EVENT),
-                ),
-                validator: _validateTextField,
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                onEditingComplete: () =>
-                    FocusScope.of(context).requestFocus(_locationNode),
+              onEditingComplete: () =>
+                  FocusScope.of(context).requestFocus(_descNode),
+            ),
+            const Divider(height: 0.0),
+            TextFormField(
+              focusNode: _descNode,
+              textInputAction: TextInputAction.next,
+              controller: _descController,
+              decoration: _buildInputDecoration(
+                OMIcons.description,
+                translations.get(StringKey.DESC_EVENT),
               ),
-              const Divider(height: 0.0),
-              TextFormField(
-                focusNode: _locationNode,
-                textInputAction: TextInputAction.next,
-                controller: _locationController,
-                decoration: _buildInputDecoration(
-                  OMIcons.locationOn,
-                  translations.get(StringKey.LOCATION_EVENT),
-                ),
-                validator: _validateTextField,
-                onEditingComplete: _onDateTap,
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+              onEditingComplete: () =>
+                  FocusScope.of(context).requestFocus(_locationNode),
+            ),
+            const Divider(height: 0.0),
+            TextFormField(
+              focusNode: _locationNode,
+              textInputAction: TextInputAction.next,
+              controller: _locationController,
+              decoration: _buildInputDecoration(
+                OMIcons.locationOn,
+                translations.get(StringKey.LOCATION_EVENT),
               ),
-              const Divider(height: 4.0),
-              ListTile(
-                leading: const Icon(OMIcons.repeat),
-                title: Text(translations.get(StringKey.EVENT_REPEAT)),
-                trailing: Switch(
-                  value: _isEventRecurrent,
-                  activeColor: theme.accentColor,
-                  onChanged: (value) {
-                    setState(() {
-                      _isEventRecurrent = value;
-                    });
-                  },
-                ),
+              onEditingComplete: _onDateTap,
+            ),
+            const Divider(height: 0.0),
+            ListTile(
+              onTap: () => _onRecurrentDate(!_isEventRecurrent),
+              leading: const Icon(OMIcons.repeat),
+              title: Text(translations.get(StringKey.EVENT_REPEAT)),
+              trailing: Switch(
+                value: _isEventRecurrent,
+                activeColor: theme.accentColor,
+                onChanged: _onRecurrentDate,
               ),
-              const Divider(height: 4.0),
-              _isEventRecurrent
-                  ? Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Wrap(
-                        spacing: 5.0,
-                        children: _buildWeekDaySelection(),
-                      ),
-                    )
-                  : ListTile(
-                      leading: const Icon(OMIcons.dateRange),
-                      title: _buildDateTimeField(
-                        translations.get(StringKey.DATE_EVENT),
-                        Date.extractDate(_eventDateStart, _locale),
-                        _onDateTap,
-                      ),
+            ),
+            const Divider(height: 0.0),
+            _isEventRecurrent
+                ? Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Wrap(
+                      spacing: 5.0,
+                      children: _buildWeekDaySelection(),
                     ),
-              const Divider(height: 4.0),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: ListTile(
-                      leading: const Icon(OMIcons.accessTime),
-                      title: _buildDateTimeField(
-                        translations.get(StringKey.START_TIME_EVENT),
-                        Date.extractTime(_eventDateStart, _locale),
-                        _onStartTimeTap,
-                      ),
+                  )
+                : ListTile(
+                    onTap: _onDateTap,
+                    leading: const Icon(OMIcons.dateRange),
+                    title: _buildDateTimeField(
+                      translations.get(StringKey.DATE_EVENT),
+                      Date.extractDate(_eventDateStart, _locale),
                     ),
                   ),
-                  Container(width: 16.0),
-                  Expanded(
-                    child: ListTile(
-                      leading: const Icon(OMIcons.accessTime),
-                      title: _buildDateTimeField(
-                        translations.get(StringKey.END_TIME_EVENT),
-                        Date.extractTime(_eventDateEnd, _locale),
-                        _onEndTimeTap,
-                      ),
+            const Divider(height: 0.0),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: ListTile(
+                    onTap: _onStartTimeTap,
+                    leading: const Icon(OMIcons.accessTime),
+                    title: _buildDateTimeField(
+                      translations.get(StringKey.START_TIME_EVENT),
+                      Date.extractTime(_eventDateStart, _locale),
                     ),
                   ),
-                ],
-              ),
-              const Divider(height: 4.0),
-              ListTile(
-                leading: const Icon(OMIcons.colorLens),
-                title: Text(translations.get(StringKey.EVENT_COLOR)),
-                trailing: Switch(
-                  value: _isCustomColor,
-                  activeColor: theme.accentColor,
-                  onChanged: (value) {
-                    setState(() {
-                      _isCustomColor = value;
-                    });
-                  },
                 ),
+                Expanded(
+                  child: ListTile(
+                    onTap: _onEndTimeTap,
+                    leading: const Icon(OMIcons.accessTime),
+                    title: _buildDateTimeField(
+                      translations.get(StringKey.END_TIME_EVENT),
+                      Date.extractTime(_eventDateEnd, _locale),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Divider(height: 0.0),
+            ListTile(
+              onTap: () => _onColorCustom(!_isCustomColor),
+              leading: const Icon(OMIcons.colorLens),
+              title: Text(translations.get(StringKey.EVENT_COLOR)),
+              trailing: Switch(
+                value: _isCustomColor,
+                activeColor: theme.accentColor,
+                onChanged: _onColorCustom,
               ),
-              _isCustomColor
-                  ? ListTileColor(
-                      title: translations.get(StringKey.EVENT_COLOR),
-                      description: translations.get(StringKey.EVENT_COLOR_DESC),
-                      selectedColor: _eventColor,
-                      onColorChange: (color) {
-                        setState(() {
-                          _eventColor = color;
-                        });
-                      },
-                    )
-                  : const SizedBox.shrink()
-            ],
-          ),
+            ),
+            _isCustomColor
+                ? ListTileColor(
+                    title: translations.get(StringKey.EVENT_COLOR),
+                    description: translations.get(StringKey.EVENT_COLOR_DESC),
+                    selectedColor: _eventColor,
+                    onColorChange: (color) {
+                      setState(() {
+                        _eventColor = color;
+                      });
+                    },
+                  )
+                : const SizedBox.shrink()
+          ],
         ),
       ),
     );
