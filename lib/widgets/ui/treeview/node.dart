eb1b6438db23e70613b1edde9cae035081383c76
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-10-23 23:48:34 +0200

    Remove useless functions (node count)

--- lib/widgets/ui/treeview/node.dart
+++ lib/widgets/ui/treeview/node.dart
@@ -10,26 +10,6 @@ class Node {
     children ??= [];
   }
 
-  int countLastChild() {
-    if (children.length == 0) return 1;
-
-    int totalChild = 0;
-    children.forEach((child) {
-      totalChild += child.countLastChild();
-    });
-
-    return totalChild;
-  }
-
-  int countNumberChildNodes() {
-    int totalChild = 1;
-    children.forEach((child) {
-      totalChild += child.countNumberChildNodes();
-    });
-
-    return totalChild;
-  }
-
   String toString() {
     if (children.length < 1)
       return "\"$key\": $value,\n";
