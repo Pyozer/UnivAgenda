e817dee5bc7a6da1fdd25fcf8c72f862f0569fbd
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-10-27 23:20:34 +0200

    Update script fetch resources script, to work after display script

--- script_fetch_resources.js
+++ script_fetch_resources.js
@@ -32,44 +32,30 @@ Node.prototype.toString = function nodeToString() {
     }
 }
 
+function pushToLevel(element, valueToPush, actualLevel, levelTarget) {
+    if (element.children == null) {
+        element.children = [];
+    }
+    let level = element.children.length - 1;
+    if (actualLevel == levelTarget)
+        element.children.push(valueToPush);
+    else {
+        pushToLevel(element.children[level < 0 ? 0 : level], valueToPush, actualLevel + 1, levelTarget);
+    }
+}
+
 function parseHTML() {
-    var rows = document.querySelectorAll('div[class^="treeline"]');
+    var rows = document.querySelectorAll('div[class^="x-grid3-row"][data-resId][data-resName]');
 
     var element = new Tree();
 
     for (var i = 0; i < rows.length; i++) {
         var row = rows[i];
 
-        var textContent = row.textContent.split('\n')[0];
-        var level = textContent.split('   ').length;
-
-        var resDivs = row.getElementsByTagName('span');
-        var resDiv = null;
-        if (resDivs.length > 1)
-            resDiv = resDivs[1];
-        else if (resDivs.length == 1) // If category (ex: Trainee)
-            resDiv = resDivs[0];
-
-        var resID = -1;
-        var spanText = "null";
-
-        if (resDiv != null) {
-            resID = resDiv.getElementsByTagName('a')[0].getAttribute("href").replace(/\D/g, "");
-            spanText = resDiv.getElementsByTagName('a')[0].textContent.replace(/"/g, '\\"');
-        }
-        pushToLevel(element, new Node(spanText, resID), 1, level);
-    }
-
-    function pushToLevel(element, valueToPush, actualLevel, levelTarget) {
-        if (element.children == null) {
-            element.children = [];
-        }
-        let level = element.children.length - 1;
-        if (actualLevel == levelTarget)
-            element.children.push(valueToPush);
-        else {
-            pushToLevel(element.children[level < 0 ? 0 : level], valueToPush, actualLevel + 1, levelTarget);
-        }
+        var level = row.getAttribute('aria-level');
+        var resID = row.getAttribute('data-resId');
+        var resName = row.getAttribute('data-resName');
+        pushToLevel(element, new Node(resName, resID), 1, level);
     }
 
     var json = element.toString().replace(/,\}/g, '}').replace(/\}\}/g, '}}');
