ca5726ea227d492552abc7f993d257684d277fde
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-11-10 00:39:41 +0100

    Improve introduction layout

--- lib/screens/introduction/intro.dart
+++ lib/screens/introduction/intro.dart
@@ -42,6 +42,16 @@ class _IntroductionScreenState extends BaseState<IntroductionScreen> {
     ]);
   }
 
+  Widget _wrapImage(Widget image) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        image,
+        const SizedBox(height: 42.0),
+      ],
+    );
+  }
+
   List<PageViewModel> _buildPages() {
     final dotActiveColor = theme.accentColor;
     final dotSize = const Size.fromRadius(6.5);
@@ -50,49 +60,53 @@ class _IntroductionScreenState extends BaseState<IntroductionScreen> {
       PageViewModel(
         translations.get(StringKey.INTRO_WELCOME_TITLE),
         translations.get(StringKey.INTRO_WELCOME_DESC),
-        Hero(tag: Asset.LOGO, child: Image.asset(Asset.LOGO, width: kIconSize)),
+        _wrapImage(
+          Hero(
+              tag: Asset.LOGO,
+              child: Image.asset(Asset.LOGO, width: kIconSize)),
+        ),
         progressSize: dotSize,
         progressColor: dotActiveColor,
       ),
       PageViewModel(
         translations.get(StringKey.INTRO_LOGIN_TITLE),
         translations.get(StringKey.INTRO_LOGIN_DESC),
-        Image.asset(Asset.INTRO_LOGIN, height: kIconSize),
+        _wrapImage(Image.asset(Asset.INTRO_LOGIN, height: kIconSize)),
         progressSize: dotSize,
         progressColor: dotActiveColor,
       ),
       PageViewModel(
         translations.get(StringKey.INTRO_AGENDA_TITLE),
         translations.get(StringKey.INTRO_AGENDA_DESC),
-        Image.asset(Asset.INTRO_GROUP, height: kIconSize),
+        _wrapImage(Image.asset(Asset.INTRO_GROUP, height: kIconSize)),
         progressSize: dotSize,
         progressColor: dotActiveColor,
       ),
       PageViewModel(
         translations.get(StringKey.INTRO_CUSTOM_TITLE),
         translations.get(StringKey.INTRO_CUSTOM_DESC),
-        Image.asset(Asset.INTRO_THEME, height: kIconSize),
+        _wrapImage(Image.asset(Asset.INTRO_THEME, height: kIconSize)),
         progressSize: dotSize,
         progressColor: dotActiveColor,
       ),
       PageViewModel(
         translations.get(StringKey.INTRO_NOTE_TITLE),
         translations.get(StringKey.INTRO_NOTE_DESC),
-        Image.asset(Asset.INTRO_NOTE, height: kIconSize),
+        _wrapImage(Image.asset(Asset.INTRO_NOTE, height: kIconSize)),
         progressSize: dotSize,
         progressColor: dotActiveColor,
       ),
       PageViewModel(
         translations.get(StringKey.INTRO_EVENT_TITLE),
         translations.get(StringKey.INTRO_EVENT_DESC),
-        Image.asset(Asset.INTRO_EVENT, height: kIconSize),
+        _wrapImage(Image.asset(Asset.INTRO_EVENT, height: kIconSize)),
         progressSize: dotSize,
         progressColor: dotActiveColor,
       ),
       PageViewModel(
         translations.get(StringKey.INTRO_OFFLINE_TITLE),
         translations.get(StringKey.INTRO_OFFLINE_DESC),
-        Image.asset(Asset.INTRO_INTERNET, height: kIconSize),
+        _wrapImage(Image.asset(Asset.INTRO_INTERNET, height: kIconSize)),
         progressSize: dotSize,
         progressColor: dotActiveColor,
       )
