c64dccf7e59f6a338ec5bfca71f6472ffd56528e
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-04-24 23:42:49 +0200

    Move to official crashlytics lib

--- lib/main.dart
+++ lib/main.dart
@@ -6,41 +6,30 @@ import 'package:myagenda/app.dart';
 import 'package:myagenda/utils/translations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:timezone/timezone.dart';
-import 'package:flutter_crashlytics/flutter_crashlytics.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 Future<List<int>> loadDefaultData() async {
-  var byteData = await rootBundle.load('packages/timezone/data/2018i_all.tzf');
+  var byteData = await rootBundle.load('packages/timezone/data/2019a_all.tzf');
   return byteData.buffer.asUint8List();
 }
 
 void main() async {
-  bool isInDebugMode = false;
+// Set `enableInDevMode` to true to see reports while in debug mode
+  // This is only to be used for confirming that reports are being
+  // submitted as expected. It is not intended to be used for everyday
+  // development.
+  Crashlytics.instance.enableInDevMode = true;
 
+  // Pass all uncaught errors to Crashlytics.
   FlutterError.onError = (FlutterErrorDetails details) {
-    if (isInDebugMode) {
-      // In development mode simply print to console.
-      FlutterError.dumpErrorToConsole(details);
-    } else {
-      // In production mode report to the application zone to report to
-      // Crashlytics.
-      Zone.current.handleUncaughtError(details.exception, details.stack);
-    }
+    Crashlytics.instance.onError(details);
   };
 
-  await FlutterCrashlytics().initialize();
+  initializeDatabase(await loadDefaultData());
+  setLocalLocation(getLocation("Europe/Paris"));
 
-  runZoned<Future<Null>>(() async {
-    initializeDatabase(await loadDefaultData());
-    setLocalLocation(getLocation("Europe/Paris"));
+  await i18n.init();
+  final prefs = await SharedPreferences.getInstance();
 
-    await i18n.init();
-    final prefs = await SharedPreferences.getInstance();
-
-    runApp(App(prefs: prefs));
-  }, onError: (error, stackTrace) async {
-    // Whenever an error occurs, call the `reportCrash` function. This will send
-    // Dart errors to our dev console or Crashlytics depending on the environment.
-    await FlutterCrashlytics()
-        .reportCrash(error, stackTrace, forceCrash: false);
-  });
+  runApp(App(prefs: prefs));
 }
